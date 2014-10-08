8593679ecf45428c38b8d105f8348884fba15aed
Evgine <aperyan.evgine@gmail.com>
2014-10-08 17:48:55 +0400

    csv task

--- apps/frontend/modules/dataload/templates/indexSuccess.php
+++ apps/frontend/modules/dataload/templates/indexSuccess.php
@@ -1,4 +1,6 @@
+<h2>Data load</h2>
+<h3>Please select a csv file and click on Upload button for importing the surveys</h3>
 <form method="post" enctype="multipart/form-data" action="dataload/upload">
     <input type="file" name="file" id="file" value="" />
-    <input type="submit" name="commit" value="Send" />
+    <input type="submit" name="commit" value="Upload" />
 </form>

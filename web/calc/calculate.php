2dfe247da6ed51f72f2842f3ac90ccec5c8f5fd9
Evgine <aperyan.evgine@gmail.com>
2015-04-09 14:32:48 +0400

    email

--- web/calc/calculate.php
+++ web/calc/calculate.php
@@ -4,106 +4,123 @@
 <link rel="stylesheet" href="../css/bootstrap.css">
 <style type="text/css">
 .green_div {
-	width: 290px; 
-	height: 250px; 
-	background-color: #569C1B;
+	/*width: 290px; */
+	min-height: 190px; 
+	background-color: #85c441;
 	color: white; 
-	font-size: 20px;
+	font-size: 16px;
 	font-weight: bold;
 	text-align: center;
 	margin: 0 auto;
+	border-radius: 8px;
 }
 </style>
 </head>
 
 <body>
-	<div class="col-sm-7 col-md-7 col-lg-7 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
+	<div class="col-sm-6 col-md-6 col-lg-6 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
 
 		<div class="form-group">
 			<div class="row">
-				<h3 style="text-align:center; font-weight:bold;">LEXTRACK PRICING CALCULATOR</h3>
+				<h3 style="text-align:center; font-weight:bold;">LexTrack PRICING CALCULATOR</h3>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
-			<form action="#" method="post" id="calc_form" name="calc">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
+				<form action="#" method="post" id="calc_form" name="calc">
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
 					<label>Firm Name:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="text" class="form-control" name="firm_name" required>
 				</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
 					<label>Number of Lawyers:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="number" class="form-control" min="1" max="5000" name="num_lawyers" required>
 				</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
 					<label>Number of Partners:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="number" class="form-control" min="1" max="5000" name="num_partners" required>
 				</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
 					<label>Number of Surveys:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="number" class="form-control" min="1" max="1000" name="num_surveys" required>
 				</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
 					<label>AmLaw Ranking:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="number" class="form-control" min="1" max="300" name="amlaw_rank" required>
 				</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
 				<div class="col-sm-3 col-md-3 col-lg-3" style="padding-right:0;">
-					<label>Avg Partner Billing Rate</label>
+					<label>Avg Partner Billing Rate:</label>
 				</div>
-				<div class="col-sm-5 col-md-5 col-lg-5">
+				<div class="col-sm-7 col-md-7 col-lg-7">
 					<input type="number" class="form-control" min="100" max="2000" name="bill_rate" required>
 				</div>
-				<div class="col-sm-4 col-md-4 col-lg-4" style="margin-top: 6px; padding:0;">
-					<div class="col-sm-2 col-md-2 col-lg-2" style="padding-right:0;">
-						<input type="checkbox" value="Use BTI 2014 Survey" id="bti_2014" name="bti_2014">
-					</div>
-					<div class="col-sm-10 col-md-10 col-lg-10" style="padding:0;">
-						<label>Use BTI 2014 Survey</label>
-					</div>
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="row">
+				<div class="col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
+					<input type="checkbox" value="Use BTI 2014 Survey" id="bti_2014" name="bti_2014">
+				</div>
+				<div class="col-sm-5 col-md-5 col-lg-5" style="padding-left:0;">
+					<label>Use BTI 2014 Survey</label>
 				</div>
 			</div>
 		</div>
 
 		<div class="form-group">
 			<div class="row" style="text-align: center;">
-				<input type="submit" class="btn btn-primary" style="width: 84px;" id="calc" name="calculate" value="Calculate">				
-				<button type="button" class="btn btn-primary" style="width: 84px;" id="reset">Reset</button> 
+				<input type="submit" class="btn btn-default" style="width: 84px; background-color:grey; color:white;" id="calc" name="calculate" value="Calculate">				
+				<button type="button" class="btn btn-default" style="width: 84px; background-color:grey; color:white;" id="reset">Reset</button> 
 			</div>
 		</form>
 		</div>
@@ -121,6 +138,9 @@
 			<input type="text" name="year_2_price" style="display:none;">
 			<input type="text" name="year_2_mins" style="display:none;">
 
+			<input type="text" name="year_1_license" style="display:none;">
+			<input type="text" name="year_2_subscription" style="display:none;">
+
 			<input type="submit" name="email" id="bl" class="btn btn-primary" value="Download Details" style="margin: 0 auto; display:none;">
 		</form>
 
@@ -133,14 +153,31 @@
 			<div class="row">
 				<div class="col-sm-6 col-md-6 col-lg-6">
 					<div class="green_div">
-						<p>Price Per Partner Per Year</p>
-						<p id="year_1">YEAR 1</p>
+						<p>YEAR 1</p>
+						<p id="year_1"></p>
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-6 col-lg-6">
 					<div class="green_div">
-						<p>Price Per Partner Per Year</p>
-						<p id="year_2">YEAR 2</p>						
+						<p>YEAR 2</p>
+						<p id="year_2"></p>						
+					</div>
+				</div>
+			</div>
+		</div>	
+
+		<div class="form-group calc_result" style="display:none;">
+			<div class="row">
+				<div class="col-sm-6 col-md-6 col-lg-6">
+					<div class="green_div">
+						<p>YEAR 1 - LICENSE</p>
+						<p id="year_1_license"></p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6">
+					<div class="green_div">
+						<p>YEAR 2 - SUBSCRIPTION</p>
+						<p id="year_2_subscription"></p>						
 					</div>
 				</div>
 			</div>
@@ -276,10 +313,10 @@ $('#calc_form').submit(function(event){
 	B2 = y2/num_partners;
 	C2_mins = (B2*60)/bill_rate;
 
-	B1 = Number((B1).toFixed(2));
-	B2 = Number((B2).toFixed(2));
-	C1_mins = Number((C1_mins).toFixed(2));
-	C2_mins = Number((C2_mins).toFixed(2));
+	B1 = Number((B1).toFixed(0));
+	B2 = Number((B2).toFixed(0));
+	C1_mins = Number((C1_mins).toFixed(0));
+	C2_mins = Number((C2_mins).toFixed(0));
 
 	$('input[name=firm_name]').val(firm_name);
 	$('input[name=lawyers_num]').val(num_lawyers);
@@ -291,13 +328,18 @@ $('#calc_form').submit(function(event){
 	$('input[name=year_1_mins]').val(C1_mins);
 	$('input[name=year_2_price]').val(B2);
 	$('input[name=year_2_mins]').val(C2_mins);
+	$('input[name=year_1_license]').val(y1);
+	$('input[name=year_2_subscription]').val(Number((y2/12).toFixed(0)));
 
 
 	
 	$('.calc_result').fadeIn();
 
-	$('#year_1').html( "<h2>$"+B1+"</h2><p>OR</p><p>"+C1_mins+" MINUTES OF A BILLABLE HOUR PER YEAR</p>" );
-	$('#year_2').html( "<h2>$"+B2+"</h2><p>OR</p><p>"+C2_mins+" MINUTES OF A BILLABLE HOUR PER YEAR</p>" );
+	$('#year_1').html( "<h1>$"+B1+"</h1><p>per partner/year</p><p>OR</p><p>"+C1_mins+" MINUTES OF BILLABLE TIME PER YEAR</p>" );
+	$('#year_2').html( "<h1>$"+B2+"</h1><p>per partner/year</p><p>OR</p><p>"+C2_mins+" MINUTES OF BILLABLE TIME PER YEAR</p>" );
+
+	$('#year_1_license').html( "<h1>$"+y1+"</h1><p>(one time)</p>" );
+	$('#year_2_subscription').html( "<h1>$"+Number((y2/12).toFixed(0))+"/mo</h1>" );
 	
 	
 
--- web/calc/file.php
+++ web/calc/file.php
@@ -3,24 +3,44 @@ ob_start();
 			if(isset($_POST['email']))
 			{
 				// the message
-				$msg = "Firm Name - ".$_POST['firm_name']."\nNumber of Lawyers - ".$_POST['lawyers_num']."\nNumber of Partners - ".$_POST['partners_num']."\nNumber of Surveys - ".$_POST['surveys_num']."\nAmLaw Ranking - ".$_POST['ranking_num']."\nAvg Partner Billing Rate - ".$_POST['billing_rate']."\nPrice Per Partner Per Year YEAR 1- $".$_POST['year_1_price']."\n".$_POST['year_1_mins']." - MINUTES OF A BILLABLE HOUR PER YEAR\nPrice Per Partner Per Year YEAR 1- $".$_POST['year_2_price']."\n".$_POST['year_2_mins']." - MINUTES OF A BILLABLE HOUR PER YEAR\n";$filename = 'calculate.txt';
-					!$handle = fopen($filename, 'w');
-					fwrite($handle, $msg);
-					fclose($handle);
+				$msg = "Firm Name - ".$_POST['firm_name']."\nNumber of Lawyers - ".$_POST['lawyers_num']."\nNumber of Partners - ".$_POST['partners_num']."\nNumber of Surveys - ".$_POST['surveys_num']."\nAmLaw Ranking - ".$_POST['ranking_num']."\nAvg Partner Billing Rate - ".$_POST['billing_rate']."\nPrice Per Partner Per Year YEAR 1- $".$_POST['year_1_price']."\n".$_POST['year_1_mins']." - MINUTES OF A BILLABLE HOUR PER YEAR\nPrice Per Partner Per Year YEAR 1- $".$_POST['year_2_price']."\n".$_POST['year_2_mins']." - MINUTES OF A BILLABLE HOUR PER YEAR\nYEAR 1 - LICENSE - $".$_POST['year_1_license']."\nYEAR 2 - SUBSCRIPTION - $".$_POST['year_2_subscription']."/mo\n";
 
-
-					header("Cache-Control: public");
-					header("Content-Description: File Transfer");
-					header("Content-Length: ". filesize("$filename").";");
-					header("Content-Disposition: attachment; filename=$filename");
-					header("Content-Type: application/octet-stream; "); 
-					header("Content-Transfer-Encoding: binary");
-				/*header("Content-type: text/plain");
-  				header("Content-Disposition: attachment; filename=savethis.txt");*/
-				// use wordwrap() if lines are longer than 70 characters
+				$date = date("dmY-Gi");
 				
-				echo $msg;
-				// send email
-				//mail("someone@example.com","My subject",$msg);	
+				$filename = "details-".$date.'.txt';
+				!$handle = fopen($filename, 'w');
+				fwrite($handle, $msg);
+				fclose($handle);
+				// multiple recipients
+				//$to  = 'george@lextrack.com'; // note the comma
+				$to  = 'aperyan.evgine@gmail.com'; // note the comma
+
+				// subject
+				$subject = 'Calculated details';
+
+				// message
+				$message = '
+				<html>
+				<head>
+				  <title>Birthday Reminders for August</title>
+				</head>
+				<body>
+				  <h1>hello</h1>
+				</body>
+				</html>
+				';
+
+				// To send HTML mail, the Content-type header must be set
+				$headers  = 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+				// Additional headers
+				$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+				$headers .= 'From: Birthday Reminder <birthday@example.com>' . "\r\n";
+				$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+				$headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+				// Mail it
+				var_dump(mail($to, $subject, $message, $headers));
+
 			}
-		?>	
+?>	

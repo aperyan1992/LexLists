849b680bc1dc7e8cccedef75d79e7562c9c00646
Evgine <aperyan.evgine@gmail.com>
2015-06-05 15:12:20 +0400

    new file

---
+++ apps/frontend/modules/surveyManagement/templates/_add_new_keyword_popup.php
@@ -0,0 +1,17 @@
+<div id="dialog_form_keyword" class="admin_popup" title="Add new keyword">
+  <form>
+    <table>
+      <tbody>
+        <tr>
+          <th>
+            <label for="survey_keyword_dialog_form_keyword" title="Survey Special Criteria.">Keyword</label>
+          </th>
+          <td>                    
+            <input type="text" id="survey_keyword_dialog_form_keyword" class="text ui-widget-content ui-corner-all" />
+            <span class="required_input">*</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
+</div>

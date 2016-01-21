3395cea7e6dbba55c12aafafaaee9419ab261ded
Aram <aram.webtech@gmail.com>
2016-01-21 12:39:55 +0400

    USA

--- web/js/survey_management.js
+++ web/js/survey_management.js
@@ -18,6 +18,26 @@ $(document).ready(function() {
     $('#s2id_lt_survey_survey_contact_id').css({"width": '434px'})
 
     $('#lt_survey_cities_list option:first-child').after('<option value="198">United States of America</option>');
+    (function(){
+        // Your base, I'm in it!
+        var originalAddClassMethod = jQuery.fn.addClass;
+
+        jQuery.fn.addClass = function(){
+            // Execute the original method.
+            var result = originalAddClassMethod.apply( this, arguments );
+
+            // trigger a custom event
+            jQuery(this).trigger('cssClassChanged');
+
+            // return the original result
+            return result;
+        }
+    })();
+
+        $("#s2id_lt_survey_cities_list").bind('cssClassChanged', function(){
+            $('.select2-drop ul li:nth-child(2) div').css('border-bottom', '1px dashed gray');
+            $('.select2-drop ul li:nth-child(2) div').css('width', '95%');
+        });
     /**
      *  Init popups
      */

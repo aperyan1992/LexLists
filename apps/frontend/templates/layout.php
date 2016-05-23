b8549d91bfe61bc52426f1a9a8dae5b3a0856c51
Aram <aram.webtech@gmail.com>
2016-05-23 18:54:45 +0400

    logo

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -44,7 +44,7 @@
             <!-- Logo -->
             <?php switch($_SERVER['HTTP_HOST']) {
                 case "23.254.166.198": $logo = "LexListsLogoDev.jpg";break;
-                case "104.168.162.106": $logo = "LexListsLogoBeta.jpg";break;
+                case "104.168.162.106": $logo = "LexListsLogoNew.jpg";break;
                 default: $logo = "LexListsLogoDev.jpg";
             } ?>
             <div id="logo" class="span3 logo_margin_left">
